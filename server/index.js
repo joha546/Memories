7ff773919b99b265b2a186cc21e97144d997dc38
Md Khaled Bin Joha <joh60483@gmail.com>
2025-04-20 22:26:25 +0600

    added folders models, controllers, routes

--- server/index.js
+++ server/index.js
@@ -2,3 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+// integrating routes.
+app.use('/posts', postRoutes);
+
+
+// MongoDb Connection String.
+const CONNECTION_URL = 'mongodb+srv://kbin3140:QTSWQluRlzWga06I@memories.axpywq8.mongodb.net/?retryWrites=true&w=majority&appName=Memories';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
+  
