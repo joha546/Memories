ec2dcf410a811636562f88a5f1ecb6ace691e7f5
Md Khaled Bin Joha <joh60483@gmail.com>
2025-04-22 01:15:01 +0600

    added api to fetch all posts and a form to create memory on homepage.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
-import memories from './images/memories.jpeg';
+import { useDispatch } from "react-redux";
 
+import {getPosts} from './actions/posts.js';
+import memories from './images/memories.jpeg';
 import Posts from './components/Posts/Posts.js';
 import Form from './components/Forms/Form.js';
 import useStyles from './styles.js';
 
 const App =() =>{
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts);
+    }, [dispatch]);
+
     return(
         <Container maxWidth='lg'>
             <AppBar className={classes.appBar} position="static" color="inherit">
---
+++ client/src/reducers/posts.js
@@ -0,0 +1,14 @@
+import { act } from "react";
+
+export default (posts = [], action) => {
+    switch (action.type) {
+        case 'FETCH_ALL':
+            return action.payload;
+
+        case 'CREATE':
+            return posts;
+    
+        default:
+            return posts;
+    }
+}
