3076b4f873f66618095c67a728edb57f6f630117
Md Khaled Bin Joha <joh60483@gmail.com>
2025-04-21 22:23:42 +0600

    added css for each component.

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 import memories from './images/memories.jpeg';
 
-import Posts from './components/Posts/Posts.js'
-import Form from './components/Forms/Form.js'
+import Posts from './components/Posts/Posts.js';
+import Form from './components/Forms/Form.js';
+import useStyles from './styles.js';
 
 const App =() =>{
+    const classes = useStyles();
     return(
         <Container maxWidth='lg'>
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">Memories</Typography>
-                <img src={memories} alt="memories" height="60" />
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className = {classes.heading} variant="h2" align="center">Memories</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60" />
             </AppBar>
 
             <Grow in>
