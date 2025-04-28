7160caff91f4944a7643fb1ae10b53b4008b0ad9
Md Khaled Bin Joha <joh60483@gmail.com>
2025-04-29 03:49:15 +0600

    feat likePost, deletePost

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,6 +5,8 @@ const API = axios.create({ baseURL: 'http://localhost:5000' });
 export const fetchPosts = () => API.get('/posts');
 export const createPost = (newPost) => API.post('/posts', newPost);
 export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
+export const deletePost = (id) => API.delete(`/posts/${id}`);
+export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 
 
 // const url = 'http://localhost:5000/posts'
