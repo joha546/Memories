9e1dd2c8daff861fc043842ff16130249662a043
Md Khaled Bin Joha <joh60483@gmail.com>
2025-05-02 12:30:23 +0600

    feat : Pagination

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,29 @@
 import React from "react";
 import { Container} from '@material-ui/core';
-import { BrowserRouter, Switch, Route } from "react-router-dom/cjs/react-router-dom.min.js";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom/cjs/react-router-dom.min.js";
 import Navbar from './components/Navbar/Navbar.js'
 import Home from './components/Home/Home.js'
 import Auth from './components/Auth/Auth.js'
+import PostDetails from "./components/PostDetails/PostDetails.jsx";
 
-const App =() =>(
+const App =() =>{
+    const user = JSON.parse(localStorage.getItem('profile'));
 
-    <BrowserRouter>
-        <Container maxWidth='lg'>
-            <Navbar />
-            <Switch>
-                <Route path='/' exact component={Home}/>
-                <Route path='/auth' exact component={Auth}/>
-            </Switch>
-        </Container>
-    </BrowserRouter>
-);
+
+    return (
+        <BrowserRouter>
+            <Container maxWidth='xl'>
+                <Navbar />
+                <Switch>
+                    <Route path='/' exact component={() => <Redirect to='/posts' />}/>
+                    <Route path='/posts' exact component={Home} />
+                    <Route path='/posts/search' exact component={Home} />   // added for search query.
+                    <Route path='/posts/:id' component={PostDetails}/>
+                    <Route path='/auth' exact component={() => (!user) ? <Auth /> : <Redirect to='/posts' /> }/>
+                </Switch>
+            </Container>
+        </BrowserRouter>
+    )
+}
 
 export default App;
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React, {useState, useEffect } from 'react'
-import { Container, Grow, Grid} from '@material-ui/core';
+import { Container, Grow, Grid, Paper} from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
 import Posts from '../Posts/Posts'
 import Form from '../Forms/Form'
 import {getPosts} from '../../actions/posts'
+import Pagination from '../Pagination.jsx';
+
 
 const Home = () => {
     
@@ -28,6 +30,9 @@ const Home = () => {
 
                <Grid item xs={12} sm={4}>
                     <Form currentId={currentId} setCurrentId={setCurrentId}/>
+                    <Paper elevation={6}>
+                        <Pagination />
+                    </Paper>
                 </Grid>
             </Grid>
         </Container>
